1600faeeeb50a276f55026d07d5e12d51221e3cb
Mark Njung'e <mark.kamau@outlook.com>
2018-02-06 19:55:01 +0300

    Change notification method
    - Sends data via payload instead of notfication object.

--- functions/index.js
+++ functions/index.js
@@ -7,42 +7,41 @@ admin.initializeApp(functions.config().firebase);
 //
 
 exports.notifyOnCompletedOrder = functions.firestore
-	.document("orders/{orderId}")
-	.onUpdate(event => {
-		return new Promise((resolve, reject) => {
-			const orderId = event.params.orderId;
-			const status = event.data.data().status;
-			const token = event.data.data().fcmToken;
+  .document("orders/{orderId}")
+  .onUpdate(event => {
+    return new Promise((resolve, reject) => {
+      const orderId = event.params.orderId;
+      const status = event.data.data().status;
+      const token = event.data.data().fcmToken;
+      const timestanp = event.data.data().timestanp;
 
-			console.log(`Update to order ${orderId}, Status: ${status}`);
+      console.log(`Update to order ${orderId}, Status: ${status}`);
 
-			if (status != "COMPLETED") {
-				resolve("Nothing to send to user.");
-				return;
-			}
+      if (status != "COMPLETED") {
+        resolve("Nothing to send to user.");
+        return;
+      }
 
-			if (token) {
-				sendNotification(
-					"Completed order",
-					`Your order has been completed.`,
-					token
-				)
-					.then(response => {
-						console.log("Notification sent to user.");
-						resolve("Notification sent to user.");
-					})
-					.catch(reason => {
-						console.log(reason);
-						reject(reason);
-					});
-			} else {
-				console.log("No token available.");
-				resolve("No token available.");
-			}
-		});
-	});
+      if (token) {
+        sendNotification(orderId, timestanp, token)
+          .then(response => {
+            console.log("Notification sent to user.");
+            resolve("Notification sent to user.");
+          })
+          .catch(reason => {
+            console.log(reason);
+            reject(reason);
+          });
+      } else {
+        console.log("No token available.");
+        resolve("No token available.");
+      }
+    });
+  });
 
-function sendNotification(title, body, token) {
-	const payload = { notification: { title, body } };
-	return admin.messaging().sendToDevice(token, payload);
+function sendNotification(orderId, timestamp, token) {
+  const payload = {
+    data: { orderId, timestamp, reason: "completed-order" }
+  };
+  return admin.messaging().sendToDevice(token, payload);
 }
