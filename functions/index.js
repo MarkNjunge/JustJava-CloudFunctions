1a2faa4f1fca6303d92d6d752901411fc4bc2bdf
Mark Njung'e <mark.kamau@outlook.com>
2018-07-25 14:09:47 +0300

    Update package structure

--- functions/index.js
+++ functions/index.js
@@ -3,41 +3,8 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-const callbackHandler = require("./callbackUrl");
-const notificationHelper = require("./notificationHelper");
+const { callbackHandler, notifyOnCompletedOrder } = require("./src");
 
-exports.notifyOnCompletedOrder = functions.firestore
-  .document("orders/{orderId}")
-  .onUpdate((change, context) => {
-    return new Promise((resolve, reject) => {
-      const orderId = context.params.orderId;
-      const status = change.after.data().status;
-      const token = change.after.data().fcmToken;
-
-      console.log(`Update to order ${orderId}, Status: ${status}`);
-
-      if (status != "COMPLETED") {
-        console.log("Nothing to send to user.");
-        resolve("Nothing to send to user.");
-        return;
-      }
-
-      if (token) {
-        notificationHelper
-          .sendOrderNotification(orderId, token)
-          .then(() => {
-            console.log("Notification sent to user.");
-            resolve("Notification sent to user.");
-          })
-          .catch(reason => {
-            console.log(reason);
-            reject(reason);
-          });
-      } else {
-        console.log("No token available.");
-        resolve("No token available.");
-      }
-    });
-  });
+exports.notifyOnCompletedOrder = notifyOnCompletedOrder;
 
 exports.callback_url = functions.https.onRequest(callbackHandler);
---
+++ functions/src/index.js
@@ -0,0 +1,41 @@
+//@ts-check
+const functions = require("firebase-functions");
+
+const callbackHandler = require("./callbackUrl");
+const notificationHelper = require("./notificationHelper");
+
+module.exports.notifyOnCompletedOrder = functions.firestore
+  .document("orders/{orderId}")
+  .onUpdate((change, context) => {
+    return new Promise((resolve, reject) => {
+      const orderId = context.params.orderId;
+      const status = change.after.data().status;
+      const token = change.after.data().fcmToken;
+
+      console.log(`Update to order ${orderId}, Status: ${status}`);
+
+      if (status != "COMPLETED") {
+        console.log("Nothing to send to user.");
+        resolve("Nothing to send to user.");
+        return;
+      }
+
+      if (token) {
+        notificationHelper
+          .sendOrderNotification(orderId, token)
+          .then(() => {
+            console.log("Notification sent to user.");
+            resolve("Notification sent to user.");
+          })
+          .catch(reason => {
+            console.log(reason);
+            reject(reason);
+          });
+      } else {
+        console.log("No token available.");
+        resolve("No token available.");
+      }
+    });
+  });
+
+module.exports.callbackHandler = callbackHandler;
